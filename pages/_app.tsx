e4e8b4085d0dac9c94ce1a9e7378b0e0c48930d8
Mario Markov <mario11septemvri@gmail.com>
2023-03-21 14:16:07 +0200

    Added localization routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,24 @@ import 'tailwindcss/tailwind.css'
 
 import { AppProps } from 'next/app'
 
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
+
+import de from "../lang/de.json";
+import en from "../lang/en.json";
+import bg from "../lang/bg.json";
+
 export default function App({ Component, pageProps }: AppProps) {
+  const { locale } = useRouter();
+  
+  const messages = {
+    de,
+    en,
+    bg,
+  };
   return (
-    <>
+    <IntlProvider locale={locale} messages={messages[locale]}>
       <Component {...pageProps} />
-    </>
+    </IntlProvider>
   )
 }
